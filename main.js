db9513f600d941a1d0aff00eab26a898943149d6
Surojit Naskar <surojitn755@gmail.com>
2026-02-23 23:34:04 +0530

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("GameHub Loaded");
+const currentUser = localStorage.getItem("currentUser");
+
+if (!currentUser) {
+    window.location.href = "login.html";
+}
+
+function logout() {
+    localStorage.removeItem("currentUser");
+    window.location.href = "login.html";
+}
+
+console.log("Logged in as:", currentUser);
